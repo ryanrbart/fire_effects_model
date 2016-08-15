5197d6378bccfadfc9ee3fb52558f6fdb1e904a2
ryanrbart <rrbart@gmail.com>
2016-08-15 16:27:22 -0700

    Added additional parameters to fire effects senesitivity test

--- code/sensitivity_batch_plots.R
+++ code/sensitivity_batch_plots.R
@@ -4,12 +4,11 @@ library("dplyr")
 library("ggplot2")
 
 
-# Import RHESSys output
+# Investigate relation between pspread and canopy height thresholds
 
 fire_sen = read.csv(file = "data_output/fire_sensitivity.csv", header = T)
 
 ls(fire_sen)
-
 
 ggplot(data = fire_sen) +
   geom_line(aes(x=pspread,y=delta_height_u), color="blue") +
@@ -17,5 +16,29 @@ ggplot(data = fire_sen) +
   facet_grid(thresh_o ~ thresh_u, labeller = label_both)
 
 
+
+
+# Investigate pspread_loss_rel parmeter
+
+fire_sen = read.csv(file = "data_output/run_2.csv", header = T)
+
+ls(fire_sen)
+
+ggplot(data = fire_sen) +
+  geom_line(aes(x=pspread_loss_rel,y=delta_lai_u), color="blue") +
+  facet_grid(. ~ pspread)
+
+
+
+
+# Investigate vapor_loss_rel parmeter
+
+fire_sen = read.csv(file = "data_output/run_3.csv", header = T)
+
+ls(fire_sen)
+
+ggplot(data = fire_sen) +
+  geom_line(aes(x=vapor_loss_rel,y=delta_litrc), color="blue") +
+  facet_grid(. ~ pspread)
 
 
