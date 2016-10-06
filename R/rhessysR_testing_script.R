7222338a351db18da5be7c26793241b442c0599d
ryanrbart <ryanrbart@eri.ucsb.edu>
2016-10-06 16:51:34 -0700

    Updated testing script for rhessysR package

--- R/rhessysR_testing_script.R
+++ R/rhessysR_testing_script.R
@@ -4,53 +4,52 @@
 
 # ---------------------------------------------------------------------
 
-rhessys_version = "/Users/ryanrbart/bin/rhessys5.20.fire_off"
-tec_file = "tecfiles/tec.p301_sim"
-world_file = "worldfiles/world.p301_patch_2canopy_dated_seq"
-world_hdr_file = "worldfiles/world.p301_patch_2canopy_dated_seq.hdr"
-flow_file = "flowtables/flow.patch"
-start_date = "1945 10 1 1"
-end_date = "1945 10 7 1"
-output_file = "out/cal5"
-output_filename = "test"
-comm_line_options = "-b"
-parameter_type = "GRID"
-m = 1.792761
-k = 1.566492
-m_v = 1.792761
-k_v = 1.566492
-pa = 7.896941
-po = 1.179359
-gw1 = 0.168035
-gw2 = 0.178753
+rhessys_version <- "/Users/ryanrbart/bin/rhessys5.20.fire_off"
+tec_file <- "tecfiles/tec.p301_sim"
+world_file <- "worldfiles/world.p301_patch_2canopy_dated_seq"
+world_hdr_file <- "worldfiles/world.p301_patch_2canopy_dated_seq.hdr"
+flow_file <- "flowtables/flow.patch"
+start_date <- "1945 10 1 1"
+end_date <- "1995 10 7 1"
+output_folder <- "out/cal5"
+output_filename <- "test"
+command_options <- "-b"
+parameter_type <- "all_combinations"
+m <- c(1.792761)
+k <- c(1.566492)
+m_v <- c(1.792761)
+k_v <- c(1.566492)
+pa <- c(7.896941)
+po <- c(1.179359)
+gw1 <- c(0.168035)
+gw2 <- c(0.178753)
 
 
-awk1 = c(15,25)
-awk2 = c(100,1000)
-awk3 = 1000
-
-# List containing awk_file, input_file, output_file
-awk1_filenames = list("awks/change.def.overstory_height_thresh.awk",
-                      "defs/veg_p301_conifer_cec.def", "defs/veg1.tmp")
-awk2_filenames = list("awks/change.def.understory_height_thresh.awk",
-                      "defs/veg_p301_conifer_cec.def", "defs/veg2.tmp")
-awk3_filenames = list("awks/change.def.pspread_loss_rel.awk",
-                      "defs/veg_p301_conifer_cec.def", "defs/veg3.tmp")
-awk_filenames = list(awk1_filenames)
+# List of lists containing parameters, awk_file, input_file, output_file
+parameter_sub_list <- list()
+parameter_sub_list[[1]] <- list(c(4,5,6),"awks/change.def.overstory_height_thresh.awk",
+                            "defs/veg_p301_conifer_cec.def", "defs/veg1.tmp")
+parameter_sub_list[[2]] <- list(c(2,3),"awks/change.def.understory_height_thresh.awk",
+                              "defs/veg_p301_conifer_cec.def", "defs/veg2.tmp")
+parameter_sub_list[[3]] <- list(c(1000),"awks/change.def.pspread_loss_rel.awk",
+                              "defs/veg_p301_conifer_cec.def", "defs/veg3.tmp")
 
 
-output_selection = list()
+# List of lists containing variable of interest, location/name of awk file (relative to output
+# file location), and the location/name of rhessys output file with variable of interest.
+output_variables <- list()
+output_variables[[1]] <- list("lai", "../../../awks/extlai.awk","../test_basin.daily")
+output_variables[[2]] <- list("et", "../../../awks/extet.awk","../test_basin.daily")
 
+# ---------------------------------------------------------------------
 
-
-run_rhessys_simulation(rhessys_version, tec_file = tec_file,
-                       world_file = world_file, world_hdr_file = world_hdr_file,
-                       flow_file = flow_file, start_date = start_date,
-                       end_date = end_date, output_file = output_file,
-                       output_filename = output_filename,
-                       comm_line_options = comm_line_options,
-                       parameter_type = parameter_type,
-                       m = m, k = k, m_v = m_v, k_v = k_v, pa = pa, po = po,
-                       gw1 = gw1, gw2 = gw2, awk_filenames=awk_filenames, awk1)
+run_rhessys(rhessys_version, tec_file = tec_file, world_file = world_file, 
+            world_hdr_file = world_hdr_file, flow_file = flow_file, 
+            start_date = start_date, end_date = end_date, 
+            output_folder = output_folder, output_filename = output_filename, 
+            command_options = command_options, parameter_type = parameter_type, 
+            m = m, k = k, m_v = m_v, k_v = k_v, pa = pa, po = po, 
+            gw1 = gw1, gw2 = gw2, parameter_sub_list = parameter_sub_list,  
+            output_variables = output_variables)
 
 
