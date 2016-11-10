b0cba3877b572237c9ebf4b06e3b6eec98f155ba
ryanrbart <ryanrbart@eri.ucsb.edu>
2016-11-09 16:16:42 -0800

    Modified files for p300 spinup

--- R/1.1_spinup_p300.R
+++ R/1.1_spinup_p300.R
@@ -5,7 +5,6 @@
 
 library(rhessysR)
 
-
 # ---------------------------------------------------------------------
 # Model inputs
 
@@ -15,10 +14,10 @@ world_file <- "ws_p300/worldfiles/world.p300_30m_2can_spinup"
 world_hdr_file <- "ws_p300/worldfiles/world.p300_30m_2can_spinup.hdr"
 flow_file <- "ws_p300/flowtables/flow.p300_30m"
 start_date <- "1941 10 1 1"
-end_date <- "1947 10 1 1"
-output_folder <- "ws_p300/out/spinup"
+end_date <- "2141 10 1 1"
+output_folder <- "ws_p300/out/p300_spinup"
 output_filename <- "spinup"
-command_options <- "-b -g -c 1 232 16064 16064"
+command_options <- "-b -g -c 1 181 9445 9445 -p 1 181 9445 9445"
 parameter_type <- "all_combinations"
 m <- c(1.792761)
 k <- c(1.566492)
@@ -33,12 +32,12 @@ gw2 <- c(0.178753)
 # List of lists containing parameters, awk_file, input_file, output_file
 parameter_change_list <- NULL
 #parameter_change_list <- list()
-#parameter_change_list[[1]] <- list(c(4,5,6),"awks/change.def.overstory_height_thresh.awk",
-#                                "defs/veg_p301_conifer_cec.def", "defs/veg1.tmp")
-#parameter_change_list[[2]] <- list(c(2),"awks/change.def.understory_height_thresh.awk",
-#                                "defs/veg_p301_conifer_cec.def", "defs/veg2.tmp")
-#parameter_change_list[[3]] <- list(c(1000),"awks/change.def.pspread_loss_rel.awk",
-#                                "defs/veg_p301_conifer_cec.def", "defs/veg3.tmp")
+#parameter_change_list[[1]] <- list(c(0.1),"awks/change.def.epc.livewood_turnover.awk",
+#                                   "ws_p300/defs/veg_p300_shrub.def", "ws_p300/defs/veg_p300_shrub.tmp1")
+#parameter_change_list[[2]] <- list(c(0.9),"awks/change.def.epc.alloc_livewoodc_woodc.awk",
+#                                   "ws_p300/defs/veg_p300_shrub.tmp1", "ws_p300/defs/veg_p300_shrub.tmp2")
+#parameter_change_list[[3]] <- list(c(0.05),"awks/change.def.epc.branch_turnover.awk",
+#                                   "ws_p300/defs/veg_p300_shrub.tmp2", "ws_p300/defs/veg_p300_shrub.tmp_final")
 
 
 # Make tec-file
@@ -46,14 +45,14 @@ parameter_change_list <- NULL
 tec_data <- data.frame(year=integer(),month=integer(),day=integer(),hour=integer(),name=character(),stringsAsFactors=FALSE)
 tec_data[1,] <- data.frame(1945, 10, 1, 1, "print_daily_on", stringsAsFactors=FALSE)
 tec_data[2,] <- data.frame(1945, 10, 1, 1, "print_daily_growth_on", stringsAsFactors=FALSE)
+tec_data[3,] <- data.frame(2091, 09, 30, 1, "output_current_state", stringsAsFactors=FALSE)
+tec_data[3,] <- data.frame(2141, 09, 30, 1, "output_current_state", stringsAsFactors=FALSE)
 
 
 # List of lists containing variable of interest, location/name of awk file (relative to output
 # file location), and the location/name of rhessys output file with variable of interest.
 output_variables <- NULL
-#output_variables <- list()
-#output_variables[[1]] <- list("lai", "../../../../awks/extlai.awk","../test_basin.daily")
-#output_variables[[2]] <- list("et", "../../../../awks/extet.awk","../test_basin.daily")
+
 
 
 # ---------------------------------------------------------------------
@@ -69,4 +68,3 @@ run_rhessys(rhessys_version, tec_file = tec_file, world_file = world_file,
             gw1 = gw1, gw2 = gw2, parameter_change_list = parameter_change_list,  
             output_variables = output_variables)
 
-beep(4)
--- R/1.2_spinup_eval.R
+++ R/1.2_spinup_eval.R
@@ -1,40 +1,106 @@
-# Evaluation of spinup
+# Spinup Evaluation
+#
+# Contains scripts for evaluating spinup.
+
+library(rhessysR)
+library(ggplot2)
 
 
 # ---------------------------------------------------------------------
-# Functions
+# Inputs
 
 
 
+OUTPUT_DIR <- "outputs"
+SPINUP_DIR <- file.path(OUTPUT_DIR, "spinup")
+
+P300_MODEL_SPINUP <- "ws_p300/out/spinup/spinup"
 
 
 # ---------------------------------------------------------------------
-# P300
+# P300 Spinup Output
+
+p300_spinup = readin_rhessys_output(P300_MODEL_SPINUP, b=1, g=1, c=1, p=1)
+
+bd = p300_spinup$bd
+bdg = p300_spinup$bdg
+pd = p300_spinup$pd
+pdg = p300_spinup$pdg
+cd = separate_canopy_output(p300_spinup$cd, 2)
+cdg = separate_canopy_output(p300_spinup$cdg, 2)
+
+# ---------------------------------------------------------------------
+# P300 Evaluation
+
+
+p300_spinup_height <- ggplot(data = cd_2) +
+  geom_line(aes(x=date,y=height, linetype=as.character(names)))
+plot(p300_spinup_height)
+ggsave("p300_spinup_height",p300_spinup_height, path = SPINUP_DIR)
+
+p300_spinup_lai <- ggplot(data = cd) +
+  geom_line(aes(x=date,y=lai, linetype=as.character(names)))
+plot(p300_spinup_lai)
+ggsave("p300_spinup_lai",p300_spinup_lai, path = SPINUP_DIR)
+
+p300_spinup_npool <- ggplot(data = cdg) +
+  geom_line(aes(x=date,y=npool, linetype=as.character(names)))
+plot(p300_spinup_npool)
+ggsave("p300_spinup_npool",p300_spinup_npool, path = SPINUP_DIR)
+
+p300_spinup_leafc <- ggplot(data = cdg) +
+  geom_line(aes(x=date,y=leafc, linetype=as.character(names)))
+plot(p300_spinup_leafc)
+ggsave("p300_spinup_leafc",p300_spinup_leafc, path = SPINUP_DIR)
+
+p300_spinup_livestemc <- ggplot(data = cdg) +
+  geom_line(aes(x=date,y=live_stemc, linetype=as.character(names)))
+plot(p300_spinup_livestemc)
+ggsave("p300_spinup_livestemc",p300_spinup_livestemc, path = SPINUP_DIR)
+
+p300_spinup_deadstemc <- ggplot(data = cdg) +
+  geom_line(aes(x=date,y=dead_stemc, linetype=as.character(names)))
+plot(p300_spinup_deadstemc)
+ggsave("p300_spinup_deadstemc",p300_spinup_deadstemc, path = SPINUP_DIR)
+
+p300_spinup_frootc <- ggplot(data = cdg) +
+  geom_line(aes(x=date,y=frootc, linetype=as.character(names)))
+plot(p300_spinup_frootc)
+ggsave("p300_spinup_frootc",p300_spinup_frootc, path = SPINUP_DIR)
+
+p300_spinup_livecrootc <- ggplot(data = cdg) +
+  geom_line(aes(x=date,y=live_crootc, linetype=as.character(names)))
+plot(p300_spinup_livecrootc)
+ggsave("p300_spinup_livecrootc",p300_spinup_livecrootc, path = SPINUP_DIR)
+
+p300_spinup_deadcrootc <- ggplot(data = cdg) +
+  geom_line(aes(x=date,y=live_crootc, linetype=as.character(names)))
+plot(p300_spinup_deadcrootc)
+ggsave("p300_spinup_deadcrootc",p300_spinup_deadcrootc, path = SPINUP_DIR)
+
+p300_spinup_cpool <- ggplot(data = cdg) +
+  geom_line(aes(x=date,y=cpool, linetype=as.character(names)))
+plot(p300_spinup_cpool)
+ggsave("p300_spinup_cpool",p300_spinup_cpool, path = SPINUP_DIR)
+
+# -------
+
+p300_spinup_soil1c <- ggplot(data = pdg) +
+  geom_line(aes(x=date,y=soil1c))
+plot(p300_spinup_soil1c)
+ggsave("p300_spinup_soil1c",p300_spinup_soil1c, path = SPINUP_DIR)
+
+p300_spinup_litr1c <- ggplot(data = pdg) +
+  geom_line(aes(x=date,y=litr1c))
+plot(p300_spinup_litr1c)
+ggsave("p300_spinup_litr1c",p300_spinup_litr1c, path = SPINUP_DIR)
 
 
 
-p300_spinup = readin_rhessys_output("ws_p300/out/spinup/spinup", b=1, g=1, c=1)
-
-ls(p300_spinup$bd)
-
-ggplot(data=p300_spinup$bd) + 
-  geom_line(aes(x=date,y=height), color="blue") 
 
 
 
-
-ls(p300_spinup$cd)
-
-ls(p300_spinup$cdg)
-
-variable = p300_spinup$cd$lai
-dates = p300_spinup$cd$date
-
-two_can = separate_canopies(variable, dates, 2)
-
-
-ggplot(data = two_can) +
-  geom_line(aes(x=dates,y=var, linetype=as.character(names)))
+# Ratio overstory to understory carbon
 
 
 # ---------------------------------------------------------------------
