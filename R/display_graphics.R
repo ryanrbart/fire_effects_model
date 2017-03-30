2ae9a907ec0225b2a75ae0aaf29402279b09e14f
ryanrbart <ryanrbart@eri.ucsb.edu>
2017-03-30 11:48:08 -0700

    Update of display graphics. Changed volatilization terminology to consumption terminology

--- R/display_graphics.R
+++ R/display_graphics.R
@@ -15,70 +15,70 @@ DISPLAY_FIGURES_DIR <- file.path(OUTPUT_DIR, "display_graphics")
 
 
 # ---------------------------------------------------------------------
-# Volatilization Output Figure
-# Relation between percent canopy loss and and percent volatilization
+# Consumption Output Figure
+# Relation between percent canopy mortality and and percent consuption
 
 
-volitalization_figure = function(vapor_loss_parameter, title_name){
+consumption_figure = function(consumption_parameter, title_name){
   
-  percent_canopy_loss <- seq(from=0,to=1,length.out=101)
+  percent_canopy_mortality <- seq(from=0,to=1,length.out=101)
   
-  tmp_func = function (percent_canopy_loss, vapor_loss_parameter){
-    if(vapor_loss_parameter == 1){
-      percent_vapor_loss <- percent_canopy_loss*100
+  tmp_func = function (percent_canopy_mortality, consumption_parameter){
+    if(consumption_parameter == 1){
+      percent_mortality <- percent_canopy_mortality*100
     }
     else {
-      percent_vapor_loss <-  (((vapor_loss_parameter^percent_canopy_loss) - 1)/(vapor_loss_parameter-1)*100)
+      percent_mortality <-  (((consumption_parameter^percent_canopy_mortality) - 1)/(consumption_parameter-1)*100)
     }
   }
   
-  percent_canopy_loss_rep <- rep(percent_canopy_loss, length(vapor_loss_parameter))
-  vapor_loss_parameter_rep <- rep(vapor_loss_parameter, each=length(percent_canopy_loss))
+  percent_canopy_mortality_rep <- rep(percent_canopy_mortality, length(consumption_parameter))
+  consumption_parameter_rep <- rep(consumption_parameter, each=length(percent_canopy_mortality))
   
-  happy <- percent_canopy_loss_rep %>%
-    cbind(percent_canopy_loss_rep = ., vapor_loss_parameter_rep) %>%
+  happy <- percent_canopy_mortality_rep %>%
+    cbind(percent_canopy_mortality_rep = ., consumption_parameter_rep) %>%
     as.data.frame()
   
-  percent_vapor_loss <- mapply(tmp_func,happy$percent_canopy_loss_rep, happy$vapor_loss_parameter_rep)
-  happy <- cbind(happy, percent_vapor_loss)
-  happy$vapor_loss_parameter_rep <- as.factor(happy$vapor_loss_parameter_rep)
+  percent_mortality <- mapply(tmp_func,happy$percent_canopy_mortality_rep, happy$consumption_parameter_rep)
+  happy <- cbind(happy, percent_mortality)
+  happy$consumption_parameter_rep <- as.factor(happy$consumption_parameter_rep)
   
   
   x <- ggplot(data = happy) +
-    geom_line(aes(x=percent_canopy_loss_rep, y=percent_vapor_loss, linetype = vapor_loss_parameter_rep, color = vapor_loss_parameter_rep), size=1.2) +
+    geom_line(aes(x=percent_canopy_mortality_rep, y=percent_mortality, linetype = consumption_parameter_rep, color = consumption_parameter_rep), size=1.2) +
     theme_bw(base_size=16) +
-    labs(title = title_name, x = "Canopy Percent Loss", y = "Percent Volatilized") +
-    scale_linetype(name = "Vapor Loss\nParameter") +
-    scale_color_brewer(palette = "Greens", name = "Vapor Loss\nParameter")
+    labs(title = title_name, x = "Canopy Mortality (%)", y = "Proportion of Mortality Consumed") +
+    scale_linetype(name = "Consumption\nParameter") +
+    scale_color_brewer(palette = "Greens", name = "Consumption\nParameter")
   plot(x)
   return(x)
 }
 
-x = volitalization_figure(vapor_loss_parameter = c(0.01, 1, 100), title_name = "Volatilization")
-ggsave("display_volatilization.pdf", plot = x, path = DISPLAY_FIGURES_DIR)
+x = consumption_figure(consumption_parameter = c(0.01, 1, 100), title_name = "Consumption")
+ggsave("display_consumption.pdf", plot = x, path = DISPLAY_FIGURES_DIR)
 
 
 
-# Volatilization Output Figures
+# Consumption Output Figures
 
-vaporization_figure = function(vapor_loss_parameter, title_name){
+consumption_output_figure = function(consumption_parameter, title_name){
 
-  percent_loss <- seq(from=0,to=1,length.out=101)
-  percent_vap <- vector()
+  percent_mortality <- seq(from=0,to=1,length.out=101)
+  percent_consumed <- vector()
   
-  for(aa in seq_along(percent_loss)){
-    if(vapor_loss_parameter == 1){
-      percent_vap[aa] <- percent_loss[aa] * percent_loss[aa]
+  for(aa in seq_along(percent_mortality)){
+    if(consumption_parameter == 1){
+      percent_consumed[aa] <- percent_mortality[aa] * percent_mortality[aa]
     }
     else {
-      percent_vap[aa] <- percent_loss[aa] * ((vapor_loss_parameter^percent_loss[aa]) - 1)/(vapor_loss_parameter-1)
+      percent_consumed[aa] <- percent_mortality[aa] * ((consumption_parameter^percent_mortality[aa]) - 1)/(consumption_parameter-1)
     }
   }
   
-  percent_litter <- percent_loss - percent_vap
+  percent_litter <- percent_mortality - percent_consumed
 
-  happy = percent_loss %>%
-    cbind(percent_vap, percent_litter) %>%
+  happy = percent_mortality %>%
+    cbind(percent_consumed, percent_litter) %>%
     `*`(100) %>%
     as.data.frame() %>%
     gather("carbon_pool", "Percent", 2:3)
@@ -86,56 +86,57 @@ vaporization_figure = function(vapor_loss_parameter, title_name){
   x <- ggplot(data = happy) +
     geom_line(aes(x=., y=Percent, linetype = carbon_pool, color = carbon_pool), size=1.2) +
     theme_bw(base_size=16) +
-    labs(title = title_name, x = "Canopy Percent Loss") +
-    scale_linetype(name = "Carbon Pool", labels=c("Litter","Volatilized")) +
-    scale_color_brewer(palette = "Dark2", name = "Carbon Pool", labels=c("Litter","Volatilized"))
+    labs(title = title_name, x = "Percent Canopy Mortality", y = "Percent of Canopy Carbon") +
+    scale_linetype(name = "Carbon Pool", labels=c("Litter","Consumed")) +
+    scale_color_brewer(palette = "Dark2", name = "Carbon Pool", labels=c("Litter","Consumed"))
   plot(x)
   return(x)
 }
 
-x = vaporization_figure(vapor_loss_parameter = 0.01, title_name = "High Volatilization")
-ggsave("volatilization_high.pdf", plot = x, path = DISPLAY_FIGURES_DIR)
-x = vaporization_figure(vapor_loss_parameter = 1, title_name = "Medium Volatilization")
-ggsave("volatilization_medium.pdf", plot = x, path = DISPLAY_FIGURES_DIR)
-x = vaporization_figure(vapor_loss_parameter = 100, title_name = "Low Volatilization")
-ggsave("volatilization_low.pdf", plot = x, path = DISPLAY_FIGURES_DIR)
+x = consumption_output_figure(consumption_parameter = 0.01, title_name = "High Consumption")
+ggsave("consumption_high.pdf", plot = x, path = DISPLAY_FIGURES_DIR)
+x = consumption_output_figure(consumption_parameter = 1, title_name = "Medium Consumption")
+ggsave("consumption_medium.pdf", plot = x, path = DISPLAY_FIGURES_DIR)
+x = consumption_output_figure(consumption_parameter = 100, title_name = "Low Consumption")
+ggsave("consumption_low.pdf", plot = x, path = DISPLAY_FIGURES_DIR)
 
 
 
 # ---------------------------------------------------------------------
-# Understory Loss Figure
-# Relation between pspread and percent loss
+# Understory Mortality Figure
+# Relation between pspread and percent mortality
 
 
-understory_figure = function(pspread_loss_parameter, title_name){
+understory_figure = function(pspread_mortality_parameter, title_name){
   
   pspread <- seq(from=0,to=1,length.out=101)
   
-  tmp_func = function (pspread, pspread_loss_parameter){
-    if(pspread_loss_parameter == 1){
-      percent_loss <- pspread*100
+  tmp_func = function (pspread, pspread_mortality_parameter){
+    if(pspread_mortality_parameter == 1){
+      percent_mortality <- pspread*100
     }
     else {
-      percent_loss <-  (((pspread_loss_parameter^pspread) - 1)/(pspread_loss_parameter-1)*100)
+      percent_mortality <-  (((pspread_mortality_parameter^pspread) - 1)/(pspread_mortality_parameter-1)*100)
     }
   }
   
-  pspread_rep <- rep(pspread, length(pspread_loss_parameter))
-  pspread_loss_parameter_rep <- rep(pspread_loss_parameter, each=length(pspread))
+  pspread_rep <- rep(pspread, length(pspread_mortality_parameter))
+  pspread_mortality_parameter_rep <- rep(pspread_mortality_parameter, each=length(pspread))
 
   happy <- pspread_rep %>%
-    cbind(pspread_rep = ., pspread_loss_parameter_rep) %>%
+    cbind(pspread_rep = ., pspread_mortality_parameter_rep) %>%
     as.data.frame()
   
-  percent_loss <- mapply(tmp_func,happy$pspread_rep, happy$pspread_loss_parameter_rep)
-  happy <- cbind(happy, percent_loss)
-  happy$pspread_loss_parameter_rep <- as.factor(happy$pspread_loss_parameter_rep)
+  percent_mortality <- mapply(tmp_func,happy$pspread_rep, happy$pspread_mortality_parameter_rep)
+  happy <- cbind(happy, percent_mortality)
+  happy$pspread_mortality_parameter_rep <- as.factor(happy$pspread_mortality_parameter_rep)
   
   
   x <- ggplot(data = happy) +
-    geom_line(aes(x=pspread_rep, y=percent_loss, linetype = pspread_loss_parameter_rep, color = pspread_loss_parameter_rep), size=1.2) +
+    geom_line(aes(x=pspread_rep, y=percent_mortality, linetype = pspread_mortality_parameter_rep, color = pspread_mortality_parameter_rep), size=1.2) +
     theme_bw(base_size=16) +
-    labs(title = title_name, x = "Pspread", y = "Understory Percent Loss") +
+#    labs(title = title_name, x = "Pspread", y = "Understory Percent Mortality") +
+    labs(title = title_name, x = "Intensity", y = "Understory Percent Mortality") +
     scale_linetype(name = "Pspread\nParameter") +
     scale_color_brewer(palette = "Greens", name = "Pspread\nParameter")
   plot(x)
@@ -143,45 +144,45 @@ understory_figure = function(pspread_loss_parameter, title_name){
 }
 
 
-x = understory_figure(pspread_loss_parameter = c(0.01, 1, 100), title_name = "Understory Loss")
+x = understory_figure(pspread_mortality_parameter = c(0.01, 1, 100), title_name = "Understory Mortality")
 ggsave("display_understory.pdf", plot = x, path = DISPLAY_FIGURES_DIR)
 
 
 
 # ---------------------------------------------------------------------
-# Overstory Loss Figure
-# Relation between understory biomass loss and overstory loss
+# Overstory Mortality Figure
+# Relation between understory biomass mortality and overstory mortality
 
 
 overstory_figure = function(k1, k2, title_name){
   
-  understory_biomass_loss <- seq(from=0,to=2, length.out = 201)   # KgC/m2
+  understory_biomass_mortality <- seq(from=0,to=2, length.out = 201)   # KgC/m2
   
-  tmp_func = function (understory_biomass_loss, k1, k2){
-    overstory_percent_loss <- (1-1/(1+exp(-(k1*(understory_biomass_loss-k2))))*100)+100
-    return(overstory_percent_loss)
+  tmp_func = function (understory_biomass_mortality, k1, k2){
+    overstory_percent_mortality <- (1-1/(1+exp(-(k1*(understory_biomass_mortality-k2))))*100)+100
+    return(overstory_percent_mortality)
   }
   
-  happy <- expand.grid(understory_biomass_loss,k1,k2)
-  happy <- dplyr::rename(happy, understory_biomass_loss_rep = Var1, k1_rep = Var2, k2_rep = Var3)
+  happy <- expand.grid(understory_biomass_mortality,k1,k2)
+  happy <- dplyr::rename(happy, understory_biomass_mortality_rep = Var1, k1_rep = Var2, k2_rep = Var3)
 
-  overstory_percent_loss <- mapply(tmp_func, happy$understory_biomass_loss_rep, happy$k1_rep, happy$k2_rep)
+  overstory_percent_mortality <- mapply(tmp_func, happy$understory_biomass_mortality_rep, happy$k1_rep, happy$k2_rep)
 
-  happy <- cbind(happy, overstory_percent_loss)
+  happy <- cbind(happy, overstory_percent_mortality)
   happy$k1_rep <- as.factor(happy$k1_rep)
   happy$k2_rep <- as.factor(happy$k2_rep)
 
   x <- ggplot(data = happy) +
-    geom_line(aes(x=understory_biomass_loss_rep, y=overstory_percent_loss, linetype = k1_rep, color = k2_rep), size=1.2) +
+    geom_line(aes(x=understory_biomass_mortality_rep, y=overstory_percent_mortality, linetype = k1_rep, color = k2_rep), size=1.2) +
     theme_bw(base_size=16) +
-    labs(title = title_name, x = "Understory Biomass Loss (Kg/m2)", y = "Overstory Percent Loss") +
+    labs(title = title_name, x = "Understory & Litter Losses (Kg/m2)", y = "Overstory Percent Mortality") +
     scale_linetype(name = "k1\nParameter") +
     scale_color_brewer(palette = "Dark2", name = "k2\nParameter")
   plot(x)
   return(x)
 }
 
-x <- overstory_figure(k1 = c(-5, -10, -50), k2 = c(0.5, 1.5), title_name = "Overstory Loss")
+x <- overstory_figure(k1 = c(-5, -10, -50), k2 = c(0.5, 1.5), title_name = "Overstory Mortality")
 ggsave("display_overstory.pdf", plot = x, path = DISPLAY_FIGURES_DIR)
 
 
