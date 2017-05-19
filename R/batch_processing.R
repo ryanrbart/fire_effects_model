a71b8a01b346b6141554b52ef1d669777782739d
ryanrbart <ryanrbart@eri.ucsb.edu>
2017-05-19 16:59:03 -0700

    Added a file to processes multiple files at once (will be handy when running multiple watersheds)

---
+++ R/batch_processing.R
@@ -0,0 +1,18 @@
+# Run scripts in batch
+
+source("R/0.1_utilities")
+
+source("R/1.1_patch_simulation_p300.R")
+source("R/1.1_patch_simulation_rs.R")
+
+source("R/1.2_patch_simulation_eval_p300.R")
+source("R/1.2_patch_simulation_eval_rs.R")
+
+
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+
+
