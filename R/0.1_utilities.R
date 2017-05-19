dd0f5bdc697e1429fe334c534f8c6b2fb4d98936
ryanrbart <ryanrbart@eri.ucsb.edu>
2017-05-19 16:55:33 -0700

    Added utilities file

---
+++ R/0.1_utilities.R
@@ -0,0 +1,46 @@
+# Utilities for fire effects analysis
+# Includes files/directories and functions
+ 
+
+# ---------------------------------------------------------------------
+# Libraries
+library(rhessysR)
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+library(lubridate)
+library(beepr)
+
+
+# ---------------------------------------------------------------------
+# Files and Directories
+
+# RHESSys outputs
+RHESSYS_DIR_p300_1.1 <- "ws_p300/out/1.1_p300_patch_simulation"
+ALLSIM_DIR_p300_1.1 <- file.path(RHESSYS_DIR_p300_1.1, "allsim")
+PARAMETER_FILE_p300_1.1<- file.path(RHESSYS_DIR_p300_1.1, "patch_sim_parameter_sets.csv")
+
+INPUT_DIR_p300_2.1 <- "ws_p300/out/2.1_p300_patch_fire"
+ALLSIM_DIR_p300_2.1 <- file.path(INPUT_DIR_p300_2.1, "allsim")
+PARAMETER_FILE_p300_2.1<- file.path(INPUT_DIR_p300_2.1, "patch_sim_parameter_sets.csv")
+
+# ---
+# R Outputs
+OUTPUT_DIR <- "outputs"
+OUTPUT_DIR_1 <- file.path(OUTPUT_DIR, "1_patch_sim")
+
+# ---
+# Display Figures
+DISPLAY_FIGURES_DIR <- file.path(OUTPUT_DIR, "display_graphics")
+
+
+# ---------------------------------------------------------------------
+# Functions
+
+y_to_wy = function(year, month, start.month=10){
+  wateryear <- ifelse(month >= start.month, year + 1, year)
+  return(wateryear)
+}
+
+
+
