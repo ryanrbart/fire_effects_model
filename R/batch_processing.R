d3511fe42008c4fef2f15a80508fa19bb459c62b
ryanrbart <ryanrbart@eri.ucsb.edu>
2018-06-11 17:17:52 -0700

    Rearranged batch processing to include sobol runs

--- R/batch_processing.R
+++ R/batch_processing.R
@@ -5,15 +5,10 @@ source("R/0.1_utilities.R")
 # ---------------------------------------------------------------------
 # Tune the model and find top parameter set
 
-source("R/1.1_patch_simulation_hja.R")
-source("R/1.1_patch_simulation_p300.R")
-source("R/1.1_patch_simulation_rs.R")
-source("R/1.1_patch_simulation_sf.R")
-
-# 
-sink(file("backup.log"), append=TRUE)
-sink(file=NULL)
-#closeAllConnections()
+source("R/1.1_patch_simulation_hja.R", echo=TRUE)
+source("R/1.1_patch_simulation_p300.R", echo=TRUE)
+source("R/1.1_patch_simulation_rs.R", echo=TRUE)
+source("R/1.1_patch_simulation_sf.R", echo=TRUE)
 
 source("R/1.2_patch_simulation_eval.R", echo=TRUE)
 
@@ -25,11 +20,26 @@ source("R/1.3_patch_export_stand_age_sf.R", echo=TRUE)
 # ---------------------------------------------------------------------
 # Sensitivity test
 
-source("R/2.1_patch_fire_p300.R")
-source("R/2.1_patch_fire_rs.R")
+source("R/2.1_sobol_ps_setup_hja.R", echo=TRUE)
+source("R/2.2_sobol_rhessys_setup_hja.R", echo=TRUE)
+source("R/2.3_sobol_rhessys_simulation_hja.R", echo=TRUE)
 
-source("R/2.2_patch_fire_eval_p300.R")
-source("R/2.2_patch_fire_eval_rs.R")
+
+source("R/2.1_sobol_ps_setup_p300.R", echo=TRUE)
+source("R/2.2_sobol_rhessys_setup_p300.R", echo=TRUE)
+source("R/2.3_sobol_rhessys_simulation_p300.R", echo=TRUE)
+
+
+source("R/2.1_sobol_ps_setup_rs.R", echo=TRUE)
+source("R/2.2_sobol_rhessys_setup_rs.R", echo=TRUE)
+source("R/2.3_sobol_rhessys_simulation_rs.R", echo=TRUE)
+
+
+source("R/2.1_sobol_ps_setup_sf.R", echo=TRUE)
+source("R/2.2_sobol_rhessys_setup_sf.R", echo=TRUE)
+source("R/2.3_sobol_rhessys_simulation_sf.R", echo=TRUE)
+
+
 
 # ---------------------------------------------------------------------
 # Patch Simulation
@@ -39,5 +49,12 @@ source("R/2.2_patch_fire_eval_rs.R")
 # ---------------------------------------------------------------------
 # Final Figures and tables
 
+
+# ---------------------------------------------------------------------
+# Sample code for sink
+# Sink directs the output from a sourced file
+sink(file("backup.log"), append=TRUE)
+sink(file=NULL)
+#closeAllConnections()
 
 
